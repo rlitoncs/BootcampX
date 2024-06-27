9e57ce339fcc3bf216b8a63539c758cf138e3e23
rlitoncs <rj_liton@hotmail.com>
2024-06-26 23:27:23 -0400

    Select Query for name of teachers and number of assistances

---
+++ 4_queries/13_name_of_teachers_and_number_of_assistances.sql
@@ -0,0 +1,16 @@
+-- Perform the same query as before, but include the number of assistances as well.
+
+SELECT
+ teachers.name AS teachers,
+ cohorts.name AS cohort,
+ COUNT(assistance_requests.*) AS total_assistances
+ FROM teachers
+  JOIN assistance_requests
+    ON teacher_id = teachers.id
+  JOIN students
+    ON student_id = students.id
+  JOIN cohorts
+    ON cohort_id = cohorts.id
+  WHERE cohorts.name = 'JUL02'
+  GROUP BY teachers.name, cohorts.name
+  ORDER BY teachers.name;
