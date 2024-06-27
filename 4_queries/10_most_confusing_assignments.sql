9f71a95da282c9f94637e6c6e0e985706af8c9f1
rlitoncs <rj_liton@hotmail.com>
2024-06-26 23:02:45 -0400

    Feat: Select Query for mostconfusing assignemnts

---
+++ 4_queries/10_most_confusing_assignments.sql
@@ -0,0 +1,15 @@
+-- List each assignment with the total number of assistance requests with it. 
+  -- Select the assignment's id, day, chapter, name and the total assistances.
+  -- Order by total assistances in order of most to least.
+
+SELECT 
+  assignments.id,
+  assignments.name,
+  assignments.day,
+  assignments.chapter,
+  COUNT(assistance_requests.*) AS total_requests
+  FROM assignments
+  JOIN assistance_requests
+    ON assignment_id = assignments.id
+  GROUP BY assignments.id
+  ORDER BY total_requests DESC;
